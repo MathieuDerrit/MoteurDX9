4f5beb3d4f5234bab7812e11b941abe6e9945b38
MAGNINAntoine <amagnin@gaming.tech>
2023-03-20 11:09:50 +0100

    Debug mouse

---
+++ Moteur/Engine.cpp
@@ -0,0 +1,35 @@
+#include "Engine.h"
+bool Engine::Initialize(HINSTANCE hInstance, std::string window_title, std::string window_class, int width, int height)
+{
+	return this->render_window.Initialize(this, hInstance, window_title, window_class, width, height);
+}
+
+bool Engine::ProcessMessages()
+{
+	return this->render_window.ProcessMessages();
+}
+
+void Engine::Update()
+{
+	while (!keyboard.CharBufferIsEmpty())
+	{
+		unsigned char ch = keyboard.ReadChar();
+	}
+
+	while (!keyboard.KeyBufferIsEmpty())
+	{
+		KeyboardEvent kbe = keyboard.ReadKey();
+		unsigned char keycode = kbe.GetKeyCode();
+	}
+
+	while (!mouse.EventBufferIsEmpty())
+	{
+		MouseEvent me = mouse.ReadEvent();
+		std::string outmsg = "X: ";
+		outmsg += std::to_string(me.GetPosX());
+		outmsg += ", Y: ";
+		outmsg += std::to_string(me.GetPosY());
+		outmsg += "\n";
+		OutputDebugStringA(outmsg.c_str());
+	}
+}
