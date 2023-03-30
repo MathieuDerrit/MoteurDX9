118fd542f9d82da730c8260d4b60f628643716cc
TheoWant <theo.wantiezfoucher@gmail.com>
2023-03-30 09:39:44 +0200

    transform

--- Moteur/Transform.cpp
+++ Moteur/Transform.cpp
@@ -11,6 +11,18 @@ Transform::Transform()
     D3DXMatrixIdentity(&m_mSca);
     D3DXMatrixIdentity(&m_mRot);
 
+    m_dir.x = 0.0f;
+    m_dir.y = 0.0f;
+    m_dir.z = 1.0f;
+
+    m_up.x = 0.0f;
+    m_up.y = 1.0f;
+    m_up.z = 0.0f;
+
+    m_right.x = 1.0f;
+    m_right.y = 0.0f;
+    m_right.z = 0.0f;
+
     updateMatrix();
 }
 
@@ -30,11 +42,11 @@ void Transform::rotate(float yaw, float pitch, float roll)
 {
     D3DXQUATERNION quat;
     D3DXQUATERNION quatRot;
-    D3DXQuaternionRotationAxis(&quat, &m_dir, roll);
+    D3DXQuaternionRotationAxis(&quat, &m_up, yaw);
     quatRot = quat;
     D3DXQuaternionRotationAxis(&quat, &m_right, pitch);
     quatRot *= quat;
-    D3DXQuaternionRotationAxis(&quat, &m_up, yaw);
+    D3DXQuaternionRotationAxis(&quat, &m_dir, roll);
     quatRot *= quat;
 
     m_quat *= quatRot;
